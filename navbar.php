9be952ba50f00e257e5854bd6cf67340b2d3b283
Coyn123 <61031960+Coyn123@users.noreply.github.com>
2025-05-01 16:21:53 -0400

    Added navigating to each page

--- navbar.php
+++ navbar.php
@@ -5,7 +5,8 @@ echo '
         <ul>
             <li><a href="../phpmyadmin/index.php">phpMyAdmin</a></li>
             <li><a href="../dashboard">Apache Dashboard</a></li>
-            <li><a href="#">TBD</a></li>
+            <li><a href="user_page.php">User Page</a></li>
+	    <li><a href="sql_query.php">Admin SQL Query</a></li>
         </ul>
     </nav>
 </header>
