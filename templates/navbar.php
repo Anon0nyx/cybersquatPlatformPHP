09e025abecc4290ba0aca58e545f5770af82a7fe
Anon0nyx <dylantforkey@gmail.com>
2025-05-04 20:55:09 -0400

    Fixed navbar links and put insert text below navbar (Navbar is always the very top of the page to maintain order across pages

--- templates/navbar.php
+++ templates/navbar.php
@@ -3,8 +3,8 @@ echo '
 <header>
     <nav>
         <ul>
-            <li><a href="../phpmyadmin/index.php">phpMyAdmin</a></li>
-            <li><a href="../dashboard">Apache Dashboard</a></li>
+            <li><a href="../../../../phpmyadmin/index.php">phpMyAdmin</a></li>
+            <li><a href="../../../../dashboard">Apache Dashboard</a></li>
             <li><a href="../user_pages/user_page.php">User Page</a></li>
 	          <li><a href="../sql_query/sql_query.php">Admin SQL Query</a></li>
             <li><a href="../user_pages/insert_page.php">Insertion Page</a></li>
